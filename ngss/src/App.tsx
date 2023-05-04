e1c52e1847f5517dadfc5b59ff3a8520b4fcc9e4
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-04 19:46:58 +0300

    added layout skeleton

--- ngss/src/App.tsx
+++ ngss/src/App.tsx
@@ -1,6 +1,28 @@
+/* PAGES */
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import Users from "./pages/Users"
+import NotFound from "./pages/NotFound"
+/* COMPONENTS */
+import SideNavi from "./components/SideNavi"
+/* ROUTE */
+import { Routes, Route } from "react-router-dom"
+/* STYLE */
+import "./styles/app.css"
+
 const App: React.FC = () => {
     return (
-        <div>app</div>
+        <main className="main-container">
+            <SideNavi />
+            <section className="content-container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </section>
+        </main>
     )
 }
 
---
+++ ngss/src/components/SideNavi.tsx
@@ -0,0 +1,10 @@
+/* STYLE */
+import "../styles/sideNavi.css"
+
+const SideNavi: React.FC = () => {
+   return (
+      <div className="navi">SideNavi</div>
+   )
+}
+
+export default SideNavi
