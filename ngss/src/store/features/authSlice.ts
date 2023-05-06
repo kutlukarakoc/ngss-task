3408ff5de6420c361cf4edb79470576e40be70fa
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-06 15:55:59 +0300

    changed colors of side navi icons

--- ngss/src/store/features/authSlice.ts
+++ ngss/src/store/features/authSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import type { RootState } from '../store';
 /* TYPES */
